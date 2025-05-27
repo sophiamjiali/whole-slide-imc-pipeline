a0d0020cfd582d9256f87e1e927a013898b8de2b
sophiamjiali <sophiamjiali@gmail.com>
2025-05-27 11:30:18 -0400

    Removed redundant intensity extraction step
    Segmentation is not possible, so everything is pixel-level. Second extraction of intensities (usually done per cell) is not necessary

--- workflow/preprocessing.py
+++ workflow/preprocessing.py
@@ -10,18 +10,84 @@ import tifffile as tf
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import scipy as scp
+import os
 
-def load_imc_tiff(filepath):
+def load_imc_image(filepath):
+    # Loads an IMC image from a TIFF file
+
+    try:
+        image = tf.imread(filepath)
+    except Exception as e:
+        file_name = os.path.basename(filepath)
+        print(f"Error loading image {file_name}: {e}")
+        return None
+
+    return image
+
+
+def load_marker_mapping(filepath):
+    # Loads marker mappings from a CSV file
+
+    return
+
+
+def create_foreground_mask(image, threshold):
+    # Defines a foreground mask, filtering out blank acquisitions
+
+    # sum intensity across all channels, 2D map of total channel
+
+    # boolean mask of pixels with summed intensity above threshold
+
+    # label foreground as 'cells', background is blank acquisition
+
+    return
+
+def save_generated_mask(mask, filepath):
+    # Saves the generated foreground mask to a file
+
+    return
+
+
+
+def extract_pixel_intensities(image, mask):
+    # Extracts pixel-level intensities from the image using the foreground mask
+
+    return
+
+
+
+
+
+def normalize_image(image, cofactors):
+    # Normalize and scale the image via winsorization and Z-score
+
+    image = 
 
     # to do
     return
 
-def normalize_image(image, cofactors = 5.0):
 
-    # to do
+def quality_control(image):
+    # Performs quality control on the image data
+
     return
 
-def sliding_window(image, channel_names, window_size = 200, step = 200):
-    """
-    Calculate marker correlations in sliding windows over a whole-slide IMC image.
-    """
+
+def filter_image(image, threshold):
+    # Filters image based on threshold value for pixel intensities
+
+    return
+
+
+
+def create_anndata(image, mask):
+    # Creates an AnnData object from the image and foreground mask
+
+    return
+
+def save_anndata(anndata, filepath):
+    # Saves the AnnData object to a file
+
+    return
+
